b290d660483beee9dac31d06587f1ed133ada591
Nivi7 <ngopalk1@uncc.edu>
2021-02-08 15:13:18 -0500

    initial commit

---
+++ coding/src/coding/practice.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package coding;
+
+/**
+ * @author nivedithag
+ *
+ */
+public class practice {
+
+	/**
+	 * 
+	 */
+	public practice() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
